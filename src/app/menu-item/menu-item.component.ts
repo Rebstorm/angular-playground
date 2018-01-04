63a4731bf1f7941c673a47e61cbb421c43f63985
Paul Jacks <paul.jacks@scopevisio.com>
2018-01-04 16:12:05 +0100

    functional feedback for list menu & nav menu

--- src/app/menu-item/menu-item.component.ts
+++ src/app/menu-item/menu-item.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuItems } from '../types/MenuItems';
 import { Router } from '@angular/router';
+import { Event } from '_debugger';
 
 @Component({
   selector: 'menu-item',
@@ -19,6 +20,7 @@ export class MenuItemComponent implements OnInit {
   
   // First thing that is called in the class. 
   constructor(private router: Router) {
+
     
   }
 
@@ -27,9 +29,48 @@ export class MenuItemComponent implements OnInit {
 
   }
 
-  selectedMenu(menu : MenuItems): void{
+  selectedFragment(menu : MenuItems, event : Event): void{
     this.router.navigate(['/'+menu.location.toLocaleLowerCase()]);
+
+    resetList(); 
+    resetMenuList();
+
+    setActive(event);
+
+    //event.target.className = "list-group-item list-group-item-action menu-list active";
+
+    function setActive(event : Event){
+      let target = event.target.id;
+      let targetComposite = target.split("-")[1];
+
+      // Menu
+      if(document.getElementById("menu-"+targetComposite) != null){
+        document.getElementById("menu-"+targetComposite).className = "nav-item active";
+      }
+
+      // List
+      if(document.getElementById("list-"+targetComposite) != null){
+        document.getElementById("list-"+targetComposite).className = "list-group-item list-group-item-action menu-list active";
+      }
+      console.log(targetComposite);
+    }
+
+    function resetList(){
+      let list = document.getElementsByClassName("list-group-item");
+      for(let i = 0; i < list.length; i++){
+        list[i].className = "list-group-item list-group-item-action menu-list";
+      }
+    }
+
+    function resetMenuList(){
+      let list = document.getElementsByClassName("nav-item");
+      for(let i = 0; i < list.length; i++){
+        list[i].className = "nav-item";
+      }
+    }
   }
+
+  
 
   
 
